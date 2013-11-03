2a5bf095ffb568a0d915c1a619a74e282bd8cef0
BatYaz <batYaz@Yazzy-and-Johnny.local>
2013-11-03 02:14:05 -0500

    Fixed unmatched html tags, eliminated excess python code in views.py

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@
 import sys, os
 
 from flask import Flask, request, render_template, redirect, url_for
-import url_key
+from url_key import generate, sanitize
 
 # initializations
 app = Flask(__name__)
@@ -18,12 +18,13 @@ def index():
     global fakeDB
     if request.method == 'POST':
 
-        longUrl = url_key.sanitize(request.form['longUrl'])
-        shortUrl = url_key.generate(longUrl)
+        longUrl = sanitize(request.form['longUrl'])
+        shortUrl = generate(longUrl)
         fakeDB[shortUrl] = longUrl
         return render_template('index.html', shortUrl=URL+shortUrl)
 
     elif request.method == 'GET':
+
         shortUrl = request.query_string
         if shortUrl in fakeDB:
             return redirect(fakeDB[shortUrl])
